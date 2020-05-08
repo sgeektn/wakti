9bdc7eace528afcd5fb994a24b270f06f8dc26a0
fakhfakh sami <s-man@MacBook-Pro-de-fakhfakh-2.local>
2020-05-09 00:15:58 +0200

    before

--- dispmail/mailgetter/urls.py
+++ dispmail/mailgetter/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('getAdress/', views.getAdress),
     path('getAdress/<str:domain>', views.getAdress),
